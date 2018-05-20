0d8e9c5212021efc8463374d68231ae12ff16762
Sam Bengtson <sam.bengtson@gmail.com>
2018-05-20 09:55:02 -0500

    remove root error

--- bootstrap/lib.sh
+++ bootstrap/lib.sh
@@ -7,9 +7,9 @@ if [[ "$BASH_VERSINFO" < 4 ]]; then
     abort 1 'You need at least bash-4.0 to run this script.'
 fi
 
-if [ "$(id -u)" = "0" ]; then
-    abort 1 'This script should NOT be started using sudo or as the root user!'
-fi
+# if [ "$(id -u)" = "0" ]; then
+#     abort 1 'This script should NOT be started using sudo or as the root user!'
+# fi
 
 if [[ -z "${HOME}" ]]; then
     abort 1 "\$HOME is not defined. Please set it first."
